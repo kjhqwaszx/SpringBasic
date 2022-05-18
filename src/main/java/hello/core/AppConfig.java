a3976e5255d779b307f17c5a0ab95487f445b188
kjhqwaszx <kjhqwaszx@naver.com>
2022-05-18 22:59:56 +0900

    add Memo

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -17,6 +17,7 @@ public class AppConfig {
     /**
      * DI(Dependency Injection) : 의존성 주입
      * AppConfig에서 의존성을 주입하면서 DIP, OCP를 만족시킨다.
+     * 각 서비스들(orderService, MemberService)에서 사용하는 구현객체들은(Rate or Fix or MemoryMember) AppConfig에서 설정해준다.
      *
      */
 
@@ -25,6 +26,7 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository());
     }
 
+    // 회원 저장방법 선택
     @Bean
     public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
@@ -35,6 +37,7 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
+    // 할인정책 선택
     @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
